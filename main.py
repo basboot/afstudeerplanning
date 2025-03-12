8d26766a2074cbf9cb51fd0c2a84a383729a175c
Bas Boot <github@basboot.nl>
2025-03-12 08:15:31 +0100

    Add students and connections

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import re
 import numpy as np
 
 teacher_availability_file = "BeschikbaarheidDocentenJuli25.xlsx"
+teacher_student_coach_file = "Afstudeerders 2024-2025.xlsx"
 
 # Assumption: teacher names are unique
 teachers = set()
@@ -16,6 +17,8 @@ days = set()
 # Assumption: same number of rooms available each day
 rooms = [f"room{i}" for i in range(3)]
 
+# TODO: coaches availability
+# TODO: expertise
 
 availability = defaultdict(set)
 
@@ -51,5 +54,37 @@ if __name__ == '__main__':
                     case _:
                         assert False, f"Illegal input {teacher} {day} {timeslot}"
 
+    # Read Excel
+    df = pd.read_excel(teacher_student_coach_file).replace(np.nan, '')
 
+
+    teacher_student = []
+    coach_student = []
+
+    students = set()
+    coaches = set()
+
+    print(teachers)
+
+    for i in range(df.shape[0]):
+        data = df.iloc[i].to_dict()
+
+        teacher = data['Afstudeerbegeleider'].split(" ")[0]
+        coach = data['Bedrijfsbegeleider']
+        student = data['Voornaam student'] + " " + data['Achternaam']
+
+        assert teacher in teachers, f"{teacher} unknown"
+        coaches.add(coach)
+        students.add(student)
+
+        coach_student.append((coach, student))
+        teacher_student.append((teacher, student))
+
+
+
+    print(availability)
+    print(f"#timeslots: {len(timeslots)}")
+    print(f"#days: {len(days)}")
+    print(f"#students: {len(students)}")
+    print(f"#coaches: {len(coaches)}")
 
